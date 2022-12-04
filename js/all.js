f3389aa9bf1cb5b483437c67071effad6bc64a9f
babu <asdfgh535060@gmail.com>
2022-12-05 00:26:03 +0800

    初始化

---
+++ js/all.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	$(".loading").ready(function(){
+		$(".item1").delay(4000).fadeOut(800);
+	});
+	$(".none").ready(function(){
+		$(".none").delay(3000).fadeIn(1000);
+	});
+});
+
+
+
+
